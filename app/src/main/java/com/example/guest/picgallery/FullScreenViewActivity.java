70ee3f74b98e475027e968017a4c5c2c70030787
archubongale <archubongale@gmail.com>
2015-10-22 17:03:32 -0700

    Added grid view

---
+++ app/src/main/java/com/example/guest/picgallery/FullScreenViewActivity.java
@@ -0,0 +1,35 @@
+package com.example.guest.picgallery;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+
+/**
+ * Created by Guest on 10/22/15.
+ */
+public class FullScreenViewActivity extends Activity{
+
+    private Utils utils;
+    private FullScreenImageAdapter adapter;
+    private ViewPager viewPager;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fullscreen_view);
+
+        viewPager=(ViewPager)findViewById(R.id.pager);
+
+        utils=new Utils(getApplicationContext());
+
+        Intent i = getIntent();
+        int position = i.getIntExtra("position", 0);
+        adapter=new FullScreenImageAdapter(FullScreenViewActivity.this, utils.getFilePaths());
+        viewPager.setAdapter(adapter);
+        viewPager.setCurrentItem(position);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/guest/picgallery/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.guest.picgallery;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.GridView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.grid_layout);
+
+        GridView gridView = (GridView) findViewById(R.id.grid_view);
+
+        // Instance of ImageAdapter Class
+        gridView.setAdapter(new ImageAdapter(this));
+    }
+
+
+}
